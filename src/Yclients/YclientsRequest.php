47f427f52444cc484f068ceefe0edca0d73cbdcd
andrey-tech <andreynero598@gmail.com>
2024-05-08 18:43:51 +0300

    Add HTTP-header "Accept"

--- src/Yclients/YclientsRequest.php
+++ src/Yclients/YclientsRequest.php
@@ -103,7 +103,10 @@ trait YclientsRequest
      */
     public function request($url, $parameters = [], $method = 'GET', $auth = true)
     {
-        $headers = [ 'Content-Type: application/json' ];
+        $headers = [
+            'Content-Type: application/json',
+            'Accept: application/vnd.yclients.v2+json'
+        ];
 
         if ($auth) {
             if (!$this->tokenPartner) {
